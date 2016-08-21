14afd706e966e2621264c484d51112a8a93fcc27
zackmichaeltech <zackmichaeltech@gmail.com>
2016-08-21 18:03:38 -0400

    Fixed indentation

--- src/routes.php
+++ src/routes.php
@@ -2,9 +2,9 @@
 // Routes
 
 $app->get('/[{name}]', function ($request, $response, $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
+  // Sample log message
+  $this->logger->info("Slim-Skeleton '/' route");
 
-    // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
+  // Render index view
+  return $this->renderer->render($response, 'index.phtml', $args);
 });
