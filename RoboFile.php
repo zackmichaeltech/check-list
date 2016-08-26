bbe67b511a3a15933a685ced345efd877e51ba27
zackmichaeltech <zackmichaeltech@gmail.com>
2016-08-25 23:49:06 -0400

    Removed commented out code

--- RoboFile.php
+++ RoboFile.php
@@ -13,10 +13,6 @@ class RoboFile extends \Robo\Tasks
    */
   public function test ($pathToSelenium = '~/selenium.jar')
   {
-    // $this->taskServer(8080)
-    //   ->background()
-    //   ->dir('public')
-    //   ->run();
     $this->taskPHPUnit()
       ->run();
   }
