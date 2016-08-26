fc1d795dddc6ffd9063af332540b8d51d5308f90
zackmichaeltech <zackmichaeltech@gmail.com>
2016-08-25 23:23:04 -0400

    Updated serve task with watch monitors

--- RoboFile.php
+++ RoboFile.php
@@ -13,10 +13,10 @@ class RoboFile extends \Robo\Tasks
    */
   public function test ($pathToSelenium = '~/selenium.jar')
   {
-    $this->taskServer(8080)
-      ->background()
-      ->dir('public')
-      ->run();
+    // $this->taskServer(8080)
+    //   ->background()
+    //   ->dir('public')
+    //   ->run();
     $this->taskPHPUnit()
       ->run();
   }
@@ -29,6 +29,18 @@ class RoboFile extends \Robo\Tasks
   {
     $this->taskServer(8080)
       ->dir('public')
+      ->background()
+      ->run();
+    $this->taskWatch()
+      ->monitor('assets', function () {
+        $this->assets();
+      })
+      ->monitor('src', function () {
+        $this->test();
+      })
+      ->monitor('tests', function () {
+        $this->test();
+      })
       ->run();
   }
 
